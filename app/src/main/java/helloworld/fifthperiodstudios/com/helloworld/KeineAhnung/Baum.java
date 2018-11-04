732a45d7e36b85d10ad23c09f6b3322561d1c428
Nils <>
2018-11-04 16:51:24 +0100

    test nils 3

--- app/src/main/java/helloworld/fifthperiodstudios/com/helloworld/KeineAhnung/Baum.java
+++ app/src/main/java/helloworld/fifthperiodstudios/com/helloworld/KeineAhnung/Baum.java
@@ -2,6 +2,7 @@ package helloworld.fifthperiodstudios.com.helloworld.KeineAhnung;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import helloworld.fifthperiodstudios.com.helloworld.R;
 
@@ -13,5 +14,7 @@ public class Baum extends AppCompatActivity {
         setContentView(R.layout.activity_baum);
         int k = 2+2*2+2;
         k = (int) Math.random()*k;
+        //nutzloser kommentar
+        Toast.makeText(this,"Nutzloser text",Toast.LENGTH_SHORT).show();
     }
 }
