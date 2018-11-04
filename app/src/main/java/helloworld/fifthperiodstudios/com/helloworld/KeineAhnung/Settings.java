f9f4dcae637c92cbf670f9a953db517122f2f4d2
Ben <>
2018-11-04 16:38:42 +0100

    Settings geändert

--- app/src/main/java/helloworld/fifthperiodstudios/com/helloworld/KeineAhnung/Settings.java
+++ app/src/main/java/helloworld/fifthperiodstudios/com/helloworld/KeineAhnung/Settings.java
@@ -2,14 +2,27 @@ package helloworld.fifthperiodstudios.com.helloworld.KeineAhnung;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 import helloworld.fifthperiodstudios.com.helloworld.R;
 
 public class Settings extends AppCompatActivity {
+    Switch s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Button b = (Button) findViewById(R.id.button);
+        s = (Switch) findViewById(R.id.switch1);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.setChecked(true);
+            }
+        });
     }
 }
