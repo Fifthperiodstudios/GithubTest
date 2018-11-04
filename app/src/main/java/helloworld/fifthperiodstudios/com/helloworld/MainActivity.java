b575a5422b2408aa3344b8e5a730d1a3c2784967
Ben <>
2018-11-04 16:10:01 +0100

    Created Project

--- app/src/main/java/helloworld/fifthperiodstudios/com/helloworld/MainActivity.java
+++ app/src/main/java/helloworld/fifthperiodstudios/com/helloworld/MainActivity.java
@@ -1,7 +1,13 @@
 package helloworld.fifthperiodstudios.com.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import helloworld.fifthperiodstudios.com.helloworld.KeineAhnung.SecondActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
